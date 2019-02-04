70852897e10850cd6d5a7a23250fca764b20ab86
Bry Cox <carl.cc@gmail.com>
2019-02-04 16:22:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -19,7 +19,7 @@ def key_for_min_value(name_hash)
     name_hash.key(arr_b[0])
   elsif arr_b[0] > arr_b[1]
     name_hash.key(arr_b[1])
-  else
+  elseif arr_b[0] == nil || arr_b[1] == nil
 
     end
 end
