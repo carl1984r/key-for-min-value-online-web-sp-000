cc2d88863ca1ab43e625328dbcfe8f4dcf96b2f1
Bry Cox <carl.cc@gmail.com>
2019-02-04 16:10:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   arr = []
   arr_b = []
-  name_hash.each {|x,y| arr << y}
+  name_hash.each {|x,y| arr << y if y != nil}
   if arr[0] < arr[1]
     arr_b << arr[0]
   else
